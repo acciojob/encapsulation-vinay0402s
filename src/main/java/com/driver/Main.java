048aea57eaafce266fe92db79e0979e25337ccf2
vinayShende <vinayjshende@gmail.com>
2023-07-24 10:37:31 +0530

    code encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+
+        RWOnly obj = new RWOnly();
+
+        obj.setName("vicky");
+
+        String ans = obj.getName();
+
+        System.out.println(ans);
+    }
 }
